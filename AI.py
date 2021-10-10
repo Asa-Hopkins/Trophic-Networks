efac8ace1ac38c465dc1148ad6d603afd260d746
Asa Hopkins <92234649+Asa-Hopkins@users.noreply.github.com>
2021-10-10 14:56:53 +0100

    Implemented Xavier initialisation
    Implemented Xavier initialisation, which removes the need for the LAMBDA hyperparameter. I've also made a new neural network class, to allow for the possibility of using the genetic algorithm to tune hyperparameters.

--- AI.py
+++ AI.py
@@ -1,111 +1,97 @@
 import numpy as np
 
-def activation(x):
+def sigmoid(x):
     return 1/(1+np.exp(-x))
+
+def Dsigmoid(x,y):
+    return x*(1-x)
+
+def tanh(x):
+    return np.tanh(x)
+
+def Dtanh(x,y):
+    return (1-x**2)
+
 def load_mnist():
     f=open("./MNIST/train-images.idx3-ubyte",'rb');images = np.frombuffer(f.read()[16:],dtype=np.uint8).reshape(60000,784)/255.0;f.close()
     f=open("./MNIST/train-labels.idx1-ubyte",'rb');labels = np.frombuffer(f.read()[8:],dtype=np.uint8);f.close()
     f=open("./MNIST/t10k-images.idx3-ubyte",'rb');test_images = np.frombuffer(f.read()[16:],dtype=np.uint8).reshape(10000,784)/255.0;f.close()
     f=open("./MNIST/t10k-labels.idx1-ubyte",'rb');test_labels = np.frombuffer(f.read()[8:],dtype=np.uint8);f.close()
-    return [images, labels, [784,10,60000,10000]], [test_images, test_labels]
+    return [images, labels, [784,10,60000]], [test_images, test_labels,[784,10,10000]]
 
 def load_balanced_emnist():
     f=open("./EMNIST/Balanced/emnist-balanced-train-images-idx3-ubyte",'rb');images = np.frombuffer(f.read()[16:],dtype=np.uint8).reshape(112800,784)/255.0;f.close()
     f=open("./EMNIST/Balanced/emnist-balanced-train-labels-idx1-ubyte",'rb');labels = np.frombuffer(f.read()[8:],dtype=np.uint8);f.close()
     f=open("./EMNIST/Balanced/emnist-balanced-test-images-idx3-ubyte",'rb');test_images = np.frombuffer(f.read()[16:],dtype=np.uint8).reshape(18800,784)/255.0;f.close()
     f=open("./EMNIST/Balanced/emnist-balanced-test-labels-idx1-ubyte",'rb');test_labels = np.frombuffer(f.read()[8:],dtype=np.uint8);f.close()
-    return [images, labels, [784,62,112800,18800]], [test_images, test_labels]
+    return [images, labels, [784,62,112800]], [test_images, test_labels,[784,62,18800]]
 
-def backprop(train,test,layout):
-    np.random.seed(42)
-    layout = [train[2][0]] + layout + [train[2][1]]
-    #layout = [784,10,10]
-    BATCH = 4
-    ETA = 100
-    LAMBDA = 0.001
-    ALPHA = 1 - 0.01
-    W = []
-    nodes= [0]
-    dnodes = [0]
-    for i in range(0,len(layout)-1):
-        W.append(np.random.random((layout[i+1],layout[i])))
-        nodes.append(0)
-        dnodes.append(0)
-    dW = [np.zeros(i.shape) for i in W]
-
-    for a in range(0,100):
-        randints = np.random.randint(train[2][2],size=BATCH*10000)
-        for i in range(0,10000):
-            #forward pass:
-            x = randints[i*BATCH:(i+1)*BATCH]
-            nodes[0] = np.transpose(train[0][x])
-            correct = train[1][x]
-            for n,i in enumerate(W):
-                nodes[n+1] = activation(LAMBDA*i.dot(nodes[n]))
-
-            target = np.zeros(layout[-1]*BATCH)
-            target[np.arange(0,BATCH*layout[-1],layout[-1])+correct]=1
-            target = np.transpose(target.reshape(BATCH,layout[-1]))
-
-       #Back propagation:
-            dnodes[-1] = 2*(nodes[-1]-target)*ETA*nodes[-1]*(1-nodes[-1])*LAMBDA
-            for i in range(len(W)-1,-1,-1):
-                dW[i] *= ALPHA
-                dW[i] += np.einsum('ji,ki->jk',dnodes[i+1], nodes[i])
-                dnodes[i] = np.transpose(W[i]).dot(dnodes[i+1])*nodes[i]*(1-nodes[i])*LAMBDA
-                W[i] -= dW[i]
-
-        nodes[0] = np.transpose(test[0])
-        for n,i in enumerate(W):
-            nodes[n+1] = activation(LAMBDA*i.dot(nodes[n]))
-        target = np.zeros(np.size(nodes[-1]))
-        target[np.arange(0,np.size(test[0],0)*np.size(nodes[-1],0),np.size(nodes[-1],0))+test[1]]=1
-        target = np.transpose(target.reshape(np.size(test[1]),layout[-1]))
-        c = np.sum(np.argmax(nodes[-1],0)==test[1])
-        print(c, c/train[2][3],np.sum((nodes[-1]-target)**2))
-
-        
 class network:
-    def __init__(self,layout, blank=False):
+    def __init__(self,layout,activation,BATCH=16,seed=42,ETA=0.00001,ALPHA = 0.99):
         self.layout = layout
-        self.input = layout[0]
-        self.output = layout[-1]
-        self.weights=[]
-        self.nodes=[0]*(len(layout))
-        self.fit = 0
-        self.correct = 0
-        self.size = 0
-        for i in range(1,len(layout)):
-            if not blank:
-                self.weights.append(np.random.random((layout[i],layout[i-1])))
-            self.size+=layout[i]*layout[i-1]
-    def activation(self,x):
-        return 1/(1+np.exp(x))
-    
-    def forward(self,data):
-        self.nodes[0] = data
-        for i in range(1,len(self.nodes)):
-            self.nodes[i] = self.activation(self.weights[i-1].dot(self.nodes[i-1]))
-        return self.nodes[-1]
+        self.ETA = ETA
+        self.BATCH = BATCH
+        self.seed = seed
+        self.ALPHA = ALPHA
+        self.best = [[1e99,0],[0,0]] #Best fitness in [0], most correct in [1], and which iteration it's achieved.
+        self.activation = activation #activation[0] is activation function, [1] is its derivative.
+        np.random.seed(seed)
+        self.W = []
+        self.nodes = [0]
+        self.dnodes = [0]
+        self.pnodes = [0] #Contains value of nodes before activation function, used for some derivatives
+        for i in range(0,len(layout)-1):
+            self.W.append(np.random.randn(layout[i+1],layout[i])*np.sqrt(1/layout[i]))
+            self.nodes.append(0)
+            self.dnodes.append(0)
+            self.pnodes.append(0)
+        self.dW = [np.zeros(i.shape) for i in self.W]
 
-    def pos(self,x):
-        n=0
-        while x>=np.size(self.weights[n]):
-            x-=np.size(self.weights[n])
-            n+=1
-        return n, x
-
-    def fitness(self,data,labels):
-        p = self.forward(data)
-        targets = np.zeros(np.size(p))
-        targets[np.arange(0,np.size(data,1)*self.output,self.output)+labels]=1
-        targets = np.transpose(targets.reshape(np.size(data,1),self.output))
-        self.fit=np.sum((targets-p)*(targets-p))
-        self.correct = np.sum(labels==np.argmax(p,0))
-        return self.fit
-            
+    def train(self, data, test_data,EPOCHS=10):
+        for a in range(EPOCHS):
+            randints = np.random.randint(data[2][2],size=(data[2][2] - data[2][2]%self.BATCH)) #train for EPOCHS, testing after each
+            for i in range(int(data[2][2]/self.BATCH)):
+                x = randints[i*self.BATCH:(i+1)*self.BATCH]
+                self.nodes[0] = np.transpose(data[0][x])
+                correct = data[1][x]
+                #Forward pass
+                for n,i in enumerate(self.W):
+                    self.pnodes[n+1] = i.dot(self.nodes[n])
+                    #print(self.nodes[n+1])
+                    self.nodes[n+1] = self.activation[0](self.pnodes[n+1])
+                    
+                target = np.zeros(self.layout[-1]*self.BATCH)
+                target[np.arange(0,self.BATCH*self.layout[-1],self.layout[-1])+correct]=1
+                target = np.transpose(target.reshape(self.BATCH,self.layout[-1]))
+                                    
+                #Back propagation:
+                self.dnodes[-1] = 2*(self.nodes[-1]-target)*self.ETA*self.activation[1](self.nodes[-1],self.pnodes[-1])
+                for i in range(len(self.W)-1,-1,-1):
+                    self.dW[i] *= self.ALPHA
+                    self.dW[i] += np.einsum('ji,ki->jk',self.dnodes[i+1], self.nodes[i])
+                    self.dnodes[i] = np.transpose(self.W[i]).dot(self.dnodes[i+1])*self.activation[1](self.nodes[i],self.pnodes[i])
+                    self.W[i] -= self.dW[i]
+                
+            self.nodes[0] = np.transpose(test_data[0])
+            for n,i in enumerate(self.W):
+                self.pnodes[n+1] = i.dot(self.nodes[n])
+                self.nodes[n+1] = self.activation[0](self.pnodes[n+1])
+            target = np.zeros(np.size(self.nodes[-1]))
+            target[np.arange(0,test_data[2][2]*np.size(self.nodes[-1],0),np.size(self.nodes[-1],0))+test_data[1]]=1
+            target = np.transpose(target.reshape(np.size(test_data[1]),self.layout[-1]))
+            c = np.sum(np.argmax(self.nodes[-1],0)==test[1])/test_data[2][2]
+            loss = np.sum((self.nodes[-1]-target)**2)
+            if loss < self.best[0][0]:
+                self.best[0] = [loss,a]
+            if c > self.best[1][0]:
+                self.best[1] = [c,a]
+            print(loss, c)
+           
 def genetic(pop,generations,data,labels,layout):
-    BATCH = 1024
+    #BATCH = [2**x for x in range(8)]
+    #ETA = [
+
+    
     x = np.random.randint(60000,size=BATCH)
     In = np.transpose(data[x]/255.0)
     correct = labels[x]
@@ -162,6 +148,4 @@ def genetic(pop,generations,data,labels,layout):
         networks = crossover(networks,pop)
         networks = mutation(networks)
         for n in networks[int(0.2 * len(networks)):]:
-            n.fitness(In,correct)
-    
-        
+            n.fitness(In,correct)        

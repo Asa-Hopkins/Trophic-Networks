e09cae50786fdfb759f0dbfb09d2e31f972334b2
Asa Hopkins <92234649+Asa-Hopkins@users.noreply.github.com>
2022-02-03 15:11:55 +0000

    Added new incoherence model
    Incoherence is now introduced by allowing the user to explicitly specify which layers each layer can connect to, giving much more control over the shape of the network whilst still allowing incoherence to appear.
    Other changes are removing AdaDelta as a method, since it hadn't been updated to allow incoherence and didn't show better performance than momentum, so was deemed not worth keeping. Also now there is annealing of the learning rate, which seems fairly hit and miss whether it seems useful or not so will likely be tweaked in the future.
    Random number seeding has been updated, the outcome is the same however the recommended method is to create a generator rather than repeatedly seeding the global one.

--- AI.py
+++ AI.py
@@ -4,8 +4,7 @@ import copy
 from scipy import sparse
 import time
 import iteround #Has methods for rounding whilst preserving sum
-#import time; train, test = load_mnist(); x = network([784,30,20,10]); t=time.time(); x.train(train,test, EPOCHS=30); print(time.time()-t)
-#train, test = load_mnist(); x = network([784,10,10], sparseMethod="SWD", sparsity=[0.1,1e-1,1e3]); x.train(train,test, EPOCHS=30);
+#train, test = load_mnist(); x = network([784,10,10], sparseMethod="SWD", sparsity=[0.1,1e-3,1]); x.train(train,test, EPOCHS=30);
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
@@ -39,8 +38,7 @@ def load_balanced_emnist():
 
 
 class network:
-    def __init__(self, layout, activation=[lambda x: np.tanh(x), lambda x,y: 1-x*x], BATCH=32, seed=42, method="Momentum", hyper=[0.9,2e-4], sparsity=0, sparseMethod="Global"):
-        t = time.time()
+    def __init__(self, layout, activation=[lambda x: np.tanh(x), lambda x,y: 1-x*x], BATCH=32, seed=42, method="Momentum", hyper=[0.9,2e-4], sparsity=0, sparseMethod="SNFS", incoherence=False):
         self.fit = 0
         self.size = 0
         self.correct = 0
@@ -48,37 +46,57 @@ class network:
         self.sparseMethod = sparseMethod
         self.method = method
         self.hyper = hyper #Contains hyperparameters for the learning method used, e.g momentum and learning rate
-        self.layout = layout
+        self.layout = np.array(layout)
+        if np.any(np.array(incoherence,dtype=object)):
+            self.incoherence = [np.array(a) for a in incoherence] #A list of lists containing which layers to connect to
+            self.out = [np.sum(self.layout[a]) for a in self.incoherence]
+        else:
+            self.incoherence = incoherence
+            self.out = self.layout[1:] #number of output nodes for each layer
         self.BATCH = BATCH
         self.seed = seed
         self.best = [[1e99, 0], [0,0], 0] #Best fitness in [0], most correct in [1], and which iteration it's achieved.
         self.activation = activation #activation[0] is activation function, [1] is its derivative.
-        np.random.seed(seed)
+        self.rng = np.random.default_rng(seed=seed)
         self.W = [] #List of weight matrices
         if sparsity:
             self.WT = [] #Transpose of that matrix (speeds up sparse calculations)
             self.dW1 = [] #Dense gradient
             self.rows = [] #Row coordinates of nonzero elements
-            self.cols = [] #Column ''
-            self.s = np.zeros(len(layout)-1)
-            self.meanMom = np.zeros(len(layout)-1)
+            self.cols = [] #Column coordinates of nonzero elements
+            self.s = np.zeros(len(layout)-1) #Size of each layer
+            self.meanMom = np.zeros(len(layout)-1) #Mean momentum for a layer, used for SNFS
+            if sparseMethod != "SWD":
+                scale = 0
+                size = 0
+                for n in range(0, len(layout)-1):
+                    scale += self.out[n]+self.layout[n]
+                    size += self.out[n]*self.layout[n]
+                scale = (np.exp(sparsity[0]) - 1)*size / scale
+                print(sparsity[0]*size)
         self.nodes = [0]
         self.dnodes = [0]
         self.pnodes = [0] #Contains value of nodes before activation function, used for some derivatives
         for n in range(0, len(layout)-1):
             if sparsity and sparseMethod!="SWD":
-                temp = int((1-sparsity[0])*(layout[n+1]+layout[n])) #Use Erdős-Rényi from Mocanu et al.
-                x,y = np.random.randint(0,layout[n+1],temp), np.random.randint(0,layout[n],temp)
-                temp = sparse.coo_matrix( (np.random.randn(temp) * np.sqrt(layout[n+1]/temp), [x,y]),(layout[n+1],layout[n]))
+                temp = int(scale*(self.out[n]+self.layout[n])) #Use Erdős-Rényi initialisation from Mocanu et al.
+                #This is lazy since it doesn't check for duplicates, it just ignores them. Calculation of scale contains a probabilistic correction to this.
+                x,y = self.rng.integers(0,self.out[n],temp), self.rng.integers(0,self.layout[n],temp)
+                temp = sparse.coo_matrix( (self.rng.standard_normal(temp) * np.sqrt(self.out[n]/temp), [x,y]),(self.out[n],self.layout[n]))
                 self.W.append(temp.tocsr())
-                print(self.W[-1].size)
-                self.dW1.append(np.zeros((layout[n+1],layout[n])))
+                self.dW1.append(np.zeros(np.shape(temp)))
                 self.WT.append(self.W[-1].T)
                 temp = np.nonzero(self.W[-1])
                 self.rows.append(temp[0])
                 self.cols.append(temp[1])
             else:
-                self.W.append(np.random.randn(layout[n+1],layout[n]) * np.sqrt(1/(layout[n])))
+                if self.incoherence:
+                    #For incoherent networks, matrix describes all nodes that this layer connects to
+                    #By doing this, backwards propagation is hardly changed, but forwards propagation is more complicated.
+                    #It's possible to instead leave forwards propagation unchanged, but this seemed the better option
+                    self.W.append(self.rng.standard_normal((self.out[n],self.layout[n])) * np.sqrt(1/(self.layout[n]*len(self.incoherence[n]))))
+                else:
+                    self.W.append(self.rng.standard_normal((self.out[n],self.layout[n])) * np.sqrt(1/(self.layout[n])))  
             self.size += np.size(self.W[-1])
             self.nodes.append(0)
             self.dnodes.append(0)
@@ -89,15 +107,24 @@ class network:
 
     def train(self, data, test_data,EPOCHS=10):
         for a in range(EPOCHS):
-            randints = np.random.randint(data[4],size=(data[4] - data[4]%self.BATCH)) #train for EPOCHS, testing after each
+            randints = self.rng.integers(data[4],size=(data[4] - data[4]%self.BATCH)) #train for EPOCHS, testing after each
             for j in range(data[4]//self.BATCH):
                 x = randints[j*self.BATCH:(j+1)*self.BATCH]
                 self.nodes[0] = data[0][x].T
                 correct = data[1][x]
                 
+                for i in range(len(self.W)):
+                    self.pnodes[i+1] = np.zeros((self.layout[i+1],self.BATCH)) #set to correct shapes
+                    self.nodes[i+1] = np.zeros((self.layout[i+1],self.BATCH))
                 #Forward pass
                 for n,i in enumerate(self.W):
-                    self.pnodes[n+1] = i.dot(self.nodes[n])
+                    if self.incoherence:
+                        temp = i.dot(self.nodes[n]) #calculate contribution to all future layers
+                        for k in self.incoherence[n]:
+                            self.pnodes[n+k] += temp[:len(self.pnodes[n+k])]
+                            temp = temp[len(self.pnodes[n+k]):]
+                    else:
+                        self.pnodes[n+1] = i.dot(self.nodes[n])
                     self.nodes[n+1] = self.activation[0](self.pnodes[n+1])
                     
                 target = np.zeros(self.layout[-1]*self.BATCH)
@@ -106,7 +133,9 @@ class network:
                                  
                 #Back propagation:
                 if self.method == "Momentum":
-                    self.dnodes[-1] = 2*(self.nodes[-1]-target)*self.activation[1](self.nodes[-1],self.pnodes[-1])*self.hyper[1]
+                    self.dnodes[-1] = 2*(self.nodes[-1]-target)*self.activation[1](self.nodes[-1],self.pnodes[-1])*self.hyper[1]*(1-2/EPOCHS)**a
+                    #includes term for annealing of learning rate, should reach about 10% of initial by the end
+                    #Changin self.hyper[1] directly caused issues with reproducibility
                     if self.sparseMethod == "SWD":
                         keep = int(self.size*self.sparsity[0]) #self.sparsity[0] is target sparsity
                         threshold = np.partition(np.hstack([np.abs(i).ravel() for i in self.W]),-keep)[-keep]
@@ -114,54 +143,48 @@ class network:
                     for n in range(len(self.W)-1,-1,-1):
                         if self.sparsity and self.sparseMethod!="SWD":
                             self.dW[n].data *= self.hyper[0]
-                            if (data[4]//self.BATCH - j < 2/self.hyper[0] and self.sparseMethod == "Global") or (data[4]//self.BATCH == j+1 and self.sparseMethod == "Local"):
+                            if (data[4]//self.BATCH - j < 2/self.hyper[0] and self.sparseMethod == "SNFS") or (data[4]//self.BATCH == j+1 and self.sparseMethod == "RigL"):
                                 self.dW1[n] *= self.hyper[0]
+                            if self.incoherence:
+                                temp = np.vstack([self.dnodes[n+k] for k in self.incoherence[n]])
+                                self.dW1[n] += temp.dot(self.nodes[n].T)
+                                self.dW[n].data += np.einsum('ik,ik->i',temp.take(self.rows[n],0),self.nodes[n].take(self.cols[n],0))
+                                self.dnodes[n] = (self.WT[n]).dot(temp)*self.activation[1](self.nodes[n],self.pnodes[n])
+                            else:
                                 self.dW1[n] += self.dnodes[n+1].dot(self.nodes[n].T)
-                            self.dW[n].data += np.einsum('ik,ik->i',self.dnodes[n+1].take(self.rows[n],0),self.nodes[n].take(self.cols[n],0))
-                                
-                            self.dnodes[n] = (self.WT[n]).dot(self.dnodes[n+1])*self.activation[1](self.nodes[n],self.pnodes[n])
+                                self.dW[n].data += np.einsum('ik,ik->i',self.dnodes[n+1].take(self.rows[n],0),self.nodes[n].take(self.cols[n],0))
+                                self.dnodes[n] = (self.WT[n]).dot(self.dnodes[n+1])*self.activation[1](self.nodes[n],self.pnodes[n])
                             self.W[n].data -= self.dW[n].data
                         else:
                             self.dW[n] *= self.hyper[0]
-                            self.dW[n] += self.dnodes[n+1].dot(self.nodes[n].T)
-                            self.dnodes[n] = (self.W[n].T).dot(self.dnodes[n+1])*self.activation[1](self.nodes[n],self.pnodes[n])
-                            #if self.sparseMethod == "SWD":
-                                #temp = self.W[n]*(self.W[n] < threshold)
-                                #self.dW[n] += self.hyper[1]*self.sparsity[1]*self.W[n]/temp
-                                #indices = np.where(np.abs(self.W[n]) < threshold)
-                                #self.dW[n] += WD*self.hyper[1]*temp#(temp/(np.linalg.norm(temp)+1e-10))
+                            if self.incoherence:
+                                temp = np.vstack([self.dnodes[n+k] for k in self.incoherence[n]])
+                                self.dW[n] += temp.dot(self.nodes[n].T)
+                                self.dnodes[n] = (self.W[n].T).dot(temp)*self.activation[1](self.nodes[n],self.pnodes[n])
+                            else:    
+                                self.dW[n] += self.dnodes[n+1].dot(self.nodes[n].T)
+                                self.dnodes[n] = (self.W[n].T).dot(self.dnodes[n+1])*self.activation[1](self.nodes[n],self.pnodes[n])
                             self.W[n] -= self.dW[n]
                             if self.sparseMethod == "SWD":
                                 temp = self.W[n]*(np.abs(self.W[n]) < threshold)
                                 self.W[n] -= temp * WD
-                
-                elif self.method == "AdaDelta":
-                    self.dnodes[-1] = 2*(self.nodes[-1]-target)*self.activation[1](self.nodes[-1],self.pnodes[-1])
-                    for n in range(len(self.W)-1,-1,-1):
-                        if self.sparsity: #By using .data it skips verifying coordinates
-                            self.rms[n].data *= self.hyper[0]
-                            self.rms[n].data += (1-self.hyper[0])*self.dW[n].data**2
-                            self.dW[n].data = np.einsum('ik,ik->i',self.dnodes[n+1][self.rows[n],:],self.nodes[n][self.cols[n],:])              
-                            self.g[n].data *= self.hyper[0]
-                            self.g[n].data += (1-self.hyper[0])*self.dW[n].data**2
-                            self.dW[n].data *= np.sqrt((self.rms[n].data+self.hyper[1])/(self.g[n].data+self.hyper[1]))
-                            self.dnodes[n] = (self.WT[n]).dot(self.dnodes[n+1])*self.activation[1](self.nodes[n],self.pnodes[n])
-                            self.W[n].data -= self.dW[n].data
-
-                        else:
-                            self.rms[n] *= self.hyper[0]
-                            self.rms[n] += (1-self.hyper[0])*self.dW[n]**2
-                            self.dW[n] = self.dnodes[n+1].dot(self.nodes[n].T)
-                            self.g[n] *= self.hyper[0]
-                            self.g[n] += (1-self.hyper[0])*self.dW[n]**2
-                            self.dW[n] *= np.sqrt((self.rms[n]+self.hyper[1])/(self.g[n]+self.hyper[1]))
-                            self.dnodes[n] = (self.W[n].T).dot(self.dnodes[n+1])*self.activation[1](self.nodes[n],self.pnodes[n])
-                            self.W[n] -= self.dW[n]
 
             #Measure performance against test set:
             self.nodes[0] = test_data[0].T
+            for i in range(len(self.W)):
+                self.pnodes[i+1] = np.zeros((self.layout[i+1],test_data[4])) #set to correct shapes
+                self.nodes[i+1] = np.zeros((self.layout[i+1],test_data[4]))
+            
             for n,i in enumerate(self.W):
-                self.nodes[n+1] = self.activation[0](i.dot(self.nodes[n]))
+                if self.incoherence:
+                    temp = i.dot(self.nodes[n]) #calculate contribution to all future layers
+                    for k in self.incoherence[n]:
+                        self.pnodes[n+k] += temp[:len(self.pnodes[n+k])]
+                        temp = temp[len(self.pnodes[n+k]):]
+                else:
+                    self.pnodes[n+1] = i.dot(self.nodes[n])
+                self.nodes[n+1] = self.activation[0](self.pnodes[n+1])
+            #print(np.sum(np.abs(self.nodes[-1])))
             target = np.zeros(test_data[4]*self.layout[-1])
             target[np.arange(0,test_data[4]*self.layout[-1],self.layout[-1])+test_data[1]]=1
             target = target.reshape(test_data[4],self.layout[-1]).T
@@ -172,29 +195,18 @@ class network:
             if c > self.best[1][0]:
                 self.best[1] = [c,a]
             self.best[2] = a
+            
             if self.sparsity and self.sparsity[1] and self.sparseMethod!="SWD": #remove self.sparsity[1] edges, introduce that many new ones
                 p = self.sparsity[1] * (1- a/EPOCHS) #Linear annealing
-                if self.sparseMethod == "Prune":
-                    #self.sparsity[1] is sparsity target
-                    matrix_data = np.concatenate([i.data for i in self.W])
-                    remove = int(self.sparsity[1]*self.size)
-                    print(remove, self.size)
-                    temp = np.argpartition(np.abs(matrix_data),remove)[:remove]
-                    matrix_data[temp] = 0 #remove lowest few values of all edges (not per layer)
-                    for n in range(len(self.W)):
-                        self.W[n].data = matrix_data[:np.size(self.W[n])]
-                        matrix_data = matrix_data[np.size(self.W[n]):]
-                        self.W[n].eliminate_zeros()
-                    self.size -= remove
                 
-                if self.sparseMethod == "Global":
+                if self.sparseMethod == "SNFS":
                     for n in range(len(self.W)):
                         self.s[n] = np.size(self.W[n]) #edges per layer before removal
                         self.meanMom[n] = np.mean(np.abs(self.dW[n].data)); #mean absolute momentum
                     self.meanMom/=np.sum(self.meanMom)
                     totalRemove = 0
                     for n in range(len(self.W)):
-                        p = np.min((p,1-self.s[n]/(self.layout[n]*self.layout[n+1])))
+                        p = np.min((p,1-self.s[n]/(self.layout[n]*self.out[n])))
                         remove = int(p*self.s[n])
                         if remove==0:
                             self.dW1[n][self.W[n].nonzero()] = 0
@@ -207,7 +219,7 @@ class network:
                         #Skipping this line accidentally causes permanent removal of edges
                     add = totalRemove*self.meanMom
                     
-                    excess = add - (np.array(self.layout)[1:]*np.array(self.layout)[:-1]) + self.s# get excess edges
+                    excess = add - (self.out*self.layout[:-1]) + self.s# get excess edges
                     while np.any(excess>0):
                         excess[np.where(excess < 0)]=-np.sum(excess[np.where(excess>0)])/np.size(excess[np.where(excess < 0)])
                         add -= excess
@@ -236,12 +248,12 @@ class network:
                         self.W[n] = sparse.lil_matrix(self.W[n])
                         self.s[n] = np.size(self.W[n]) #remaining edges per layer
                         while self.s[n]>0: #Need to be careful about picking already nonzero elements when choosing new edges to add
-                            x, y = np.random.randint(0,self.layout[n+1]), np.random.randint(0,self.layout[n]) #not an idea solution but shouldn't be a huge bottleneck
+                            x, y = self.rng.integers(0,self.out[n]), self.rng.integers(0,self.layout[n]) #not an ideal solution but shouldn't be a huge bottleneck
                             if self.W[n][x,y] == 0:
                                 self.W[n][x,y] = 1e-10
                                 self.s[n]-=1
 
-                    elif self.sparseMethod == "Local":
+                    elif self.sparseMethod == "RigL":
                         remove = int(self.s[n]*p)
                         temp = np.argpartition(self.W[n].data,remove)[:remove]
                         self.W[n].data[temp] = 0
@@ -258,6 +270,6 @@ class network:
                     self.dW[n].data *= 0
                     self.dW1[n] *= 0
                     self.cols[n], self.rows[n], _ = sparse.find(self.WT[n])
-            self.hyper[1] *= 1-2/EPOCHS #Decay learning rate, should be around 10% of initial by the end
+            #    self.hyper[1] *= 1-2/EPOCHS #Decay learning rate, should be around 10% of initial by the end
             print(loss, c, [np.size(i) for i in self.W], np.sum([np.size(i) for i in self.W]))
         return c
